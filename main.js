863236af9285529be91d477c028dbd9ed830b757
wen <jwcastillo@gmail.com>
2022-11-11 17:12:55 -0300

    chore: add stage and thresholds github action

--- main.js
+++ main.js
@@ -6,6 +6,21 @@ import { updateAddress } from "./updateAddress.js";
 import { submitCheckout } from "./submitCheckout.js";
 
 export const options = {
+  stages: [
+    // Ramp-up from 1 to 5 VUs in 10s
+    { duration: "30s", target: 5 },
+
+    // Stay at rest on 5 VUs for 5s
+    { duration: "15s", target: 5 },
+
+    // Ramp-down from 5 to 0 VUs for 5s
+    { duration: "5s", target: 0 }
+  ],
+
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
 };
 
 // used to store global variables
